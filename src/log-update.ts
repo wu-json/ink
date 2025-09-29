5b64a29130a623a2baf7de663e0941c3cbad5f23
Jason Wu <jason.wu2153@gmail.com>
2025-09-29 15:34:14 -0700

    feat: incremental rendering

--- src/log-update.ts
+++ src/log-update.ts
@@ -39,6 +39,8 @@ const create = (stream: Writable, {showCursor = false} = {}): LogUpdate => {
 		// Clear any lines if necessary (only if output has less lines than previous)
 		if (lineCount < previousLineCount) {
 			stream.write(ansiEscapes.eraseLines(previousLineCount - lineCount) + ansiEscapes.cursorUp(lineCount - 1))
+		}else {
+			stream.write(ansiEscapes.cursorUp(previousLineCount))
 		}
 
 		for (let i = 0; i < lineCount; i++) {
